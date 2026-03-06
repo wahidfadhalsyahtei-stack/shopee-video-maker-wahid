b94cf61d7c8b5af7354207e1c0051b6d6e65ffd0
wahidfadhalsyahtei-stack <wahidfadhalsyah.tei@gmail.com>
2026-03-07 06:18:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+let images = [
+"https://via.placeholder.com/600",
+"https://via.placeholder.com/600/ff5722",
+"https://via.placeholder.com/600/000"
+]
+
+let index = 0
+let recorder
+let chunks = []
+
+function generateVideo(){
+
+let stream = canvas.captureStream(30)
+
+recorder = new MediaRecorder(stream)
+
+recorder.ondataavailable = e => chunks.push(e.data)
+
+recorder.onstop = exportVideo
+
+recorder.start()
+
+let interval = setInterval(()=>{
+
+let img = new Image()
+
+img.src = images[index]
+
+img.onload = ()=>{
+
+ctx.clearRect(0,0,canvas.width,canvas.height)
+
+ctx.drawImage(img,60,60,600,600)
+
+ctx.font="40px Arial"
+ctx.fillText("Produk Viral Shopee",150,50)
+
+}
+
+index++
+
+if(index >= images.length){
+
+clearInterval(interval)
+
+setTimeout(()=> recorder.stop(),1000)
+
+}
+
+},2000)
+
+}
+
+function exportVideo(){
+
+let blob = new Blob(chunks,{type:"video/mp4"})
+
+window.videoURL = URL.createObjectURL(blob)
+
+}
+
+function downloadVideo(){
+
+if(window.videoURL){
+
+let a = document.createElement("a")
+
+a.href = window.videoURL
+
+a.download = "video-produk.mp4"
+
+a.click()
+
+}
+
+}
